6bbd327f2d607610ccec432599dab46aea36f29c
nghia <trongnghialedinh@gmail.com>
2021-05-27 23:30:58 +0700

    fix walking animation

--- 05-ScenceManager/Mario.h
+++ 05-ScenceManager/Mario.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "GameObject.h"
 
-#define MARIO_WALKING_SPEED		0.15f 
+#define MARIO_WALKING_SPEED		0.1f 
 //0.1f
 #define MARIO_JUMP_SPEED_Y		0.5f
 #define MARIO_JUMP_DEFLECT_SPEED 0.2f
