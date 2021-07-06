8ee5a278cab5f35ad20513cbf9dda371bf77ce82
Trong Nghia <trongnghialedinh@gmail.com>
2021-07-06 21:46:39 +0700

    modify question brick hop up distance

--- 05-ScenceManager/QBrick.cpp
+++ 05-ScenceManager/QBrick.cpp
@@ -87,7 +87,7 @@ void QBrick::HopUpABit()
 	if (state == QBRICK_STATE_MYSTERIOUS) {
 		oldY = y;
 		//vx = -0.5f;
-		y -= 1.5f;
+		y -= 3.0f;
 		state = QBRICK_STATE_NO_MORE_MYSTERIOUS;
 		start_hopUp = GetTickCount();
 		hopUp = 1;
